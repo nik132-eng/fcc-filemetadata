4a32f984d8405919b74899be459891a412beb644
nik132-eng <nikunjrohit10@gmail.com>
2024-01-28 15:50:25 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer  = require('multer');
+
+// Set up multer storage
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 var app = express();
 
@@ -11,8 +16,23 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+// Set up a route for file upload
+app.post('/api/fileanalyse', upload.single('file'), (req, res) => {
+  // Extract file information
+  const file = req.file;
+  const fileName = file.originalname;
+  const fileType = file.mimetype;
+  const fileSize = file.size;
 
+  // Construct response data
+  const responseData = {
+      name: fileName,
+      type: fileType,
+      size: fileSize
+  };
 
+  res.json(responseData);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
